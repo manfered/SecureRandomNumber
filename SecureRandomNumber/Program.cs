7f5fb99ab20b8a03f573bb17da8cc860ff776b7e
manfered <35757736+manfered@users.noreply.github.com>
2018-08-04 12:26:35 -0600

    Add project files.

---
+++ SecureRandomNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureRandomNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Random numbers");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine();
+
+            for(var i = 0; i < 10; ++i)
+            {
+                Console.WriteLine($"Random number {i} : {Convert.ToBase64String(SecureRandom.GenerateRandomNumber(32))}");
+            }
+        }
+    }
+}
---
+++ SecureRandomNumber/SecureRandom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureRandomNumber
+{
+    public class SecureRandom
+    {
+        public static byte[] GenerateRandomNumber(int length)
+        {
+            using (var RandomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[length];
+                RandomNumberGenerator.GetBytes(randomNumber);
+
+                return randomNumber;
+            }
+        }
+    }
+}
